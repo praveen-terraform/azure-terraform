5333042cd2dbd946477ec7b29ad22dcccdf3fafe
PraveenChaswika <91453923+PraveenChaswika@users.noreply.github.com>
2026-04-11 13:17:52 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "backendsatfstate"
-    container_name       = "tfstate"
-    key                  = "devenv.terraform.tfstate"
+    container_name       = "terraformstatecontainer"
+    key                  = "dev.terraform.tfstate"
   }
 }
